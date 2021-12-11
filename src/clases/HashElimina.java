00b1662233ff823a69ba9a5fb32405dea2c685c2
Rogelio OM <>
2021-12-11 17:05:32 -0600

    Inserción de la clase HashElimina. ROM.

---
+++ src/clases/HashElimina.java
@@ -0,0 +1,15 @@
+package clases;
+
+import static clases.Hash.buscaHash;
+
+public class HashElimina{
+    public static int eliminaHash(Hash[] h, int m, int n) {
+        int i = buscaHash(h, m, n);
+        if (i == -1) {
+            return -1;
+        } else {
+            h[i].estado = 1;
+            return 1;
+        }
+    }
+}
