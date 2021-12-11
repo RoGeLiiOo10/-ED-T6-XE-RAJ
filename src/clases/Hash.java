d4cb8235169031312f754d35ffa85e8b306e8c57
Rogelio OM <>
2021-12-11 16:52:09 -0600

    Inserción del programa de tablas Hash, AARC, JJMT, ROM.

---
+++ src/clases/Hash.java
@@ -0,0 +1,57 @@
+package clases;
+import pruebas.PruebaHash;
+public class Hash{
+    public int dato;
+    public int estado; //0 = Vacío, 1 = Eliminado, 2 = Ocupado
+
+    public static int funcion(int n, int m) {
+        return ((n + 1) % m);
+    }
+
+    public static void insertaHash(Hash[] h, int m, int n) {
+        boolean i = false;
+        int j = funcion(n, m);
+        do {
+            if (h[j].estado == 0 || h[j].estado == 1) {
+                h[j].dato = n;
+                h[j].estado = 2;
+                i = true;
+            } else {
+                j++;
+            }
+        } while (j < m && !i);
+        if (i) {
+            javax.swing.JOptionPane.showMessageDialog(null, "¡Elemento insertado con éxito!");
+        } else {
+            javax.swing.JOptionPane.showMessageDialog(null, "¡Tabla llena!");
+        }
+    }
+
+    public static int buscaHash(Hash[] h, int m, int n) {
+        int j = funcion(n, m);
+        while (j < m) {
+            if (h[j].estado == 0) {
+                return -1;
+            } else if (h[j].dato == n) {
+                if (h[j].estado == 1) {
+                    return -1;
+                } else {
+                    return j;
+                }
+            } else {
+                j++;
+            }
+        }
+        return -1;
+    }
+
+    public static int eliminaHash(Hash[] h, int m, int n) {
+        int i = buscaHash(h, m, n);
+        if (i == -1) {
+            return -1;
+        } else {
+            h[i].estado = 1;
+            return 1;
+        }
+    }
+}
